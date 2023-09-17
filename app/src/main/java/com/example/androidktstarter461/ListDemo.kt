e8e9d49f59374aaddecde15fb5f08929d33af25d
Yao Long <yaolongt@gmail.com>
2023-09-17 17:13:54 +0800

    added code for listview and spinner

--- app/src/main/java/com/example/androidktstarter461/ListDemo.kt
+++ app/src/main/java/com/example/androidktstarter461/ListDemo.kt
@@ -2,13 +2,92 @@ package com.example.androidktstarter461
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Spinner
+import java.util.Scanner
 
 class ListDemo : AppCompatActivity() {
+    private lateinit var listAdapter: ArrayAdapter<String>
+    private lateinit var spinnerAdapter: ArrayAdapter<String>
+
+    private var wordsMap = HashMap<String, ArrayList<String>>()
+    private var words = ArrayList<String>()
+    private var displayList = ArrayList<String>()
+
+    private lateinit var selectedWord: String
+    private var selectedWordList = ArrayList<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_demo)
+
+        val scanner1 = Scanner(resources.openRawResource(R.raw.help))
+        readFile(scanner1)
+
+        val wordSpinner = findViewById<Spinner>(R.id.spinner)
+        spinnerAdapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, words)
+        wordSpinner.adapter = spinnerAdapter
+
+        wordSpinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            // if an item is selected
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                selectedWord = p0!!.getItemAtPosition(p2).toString()
+                setUpList() // fixed error
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
     }
 
+    private fun readFile(scanner: Scanner) {
+        while(scanner.hasNextLine()) {
+            val line = scanner.nextLine()
+            val pieces = line.split("\t")
+//
+//            // split a string of words by comma
+            val wordDesc = pieces[1].split(",")
+            Log.d("wordDesc", wordDesc.toString())
+//
+            words.add(pieces[0])
+            wordsMap[pieces[0]] = wordDesc as ArrayList<String>
+        }
+    }
 
+    private fun setUpList() {
+        displayList.clear()
 
+        displayList = wordsMap[selectedWord]!!
+
+        listAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, displayList)
+        val wordsListView = findViewById<ListView>(R.id.listView)
+        wordsListView.adapter = listAdapter
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        selectedWord = spinner.selectedItem.toString()
+
+        // use key-value pair to save the values during runtime and pass arnd?
+        outState.putString("selectedWord", selectedWord)
+        outState.putStringArrayList("displayList", displayList)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        selectedWord = savedInstanceState.getString("selectedWord").toString()
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        spinner.adapter = spinnerAdapter
+        spinner.setSelection(spinnerAdapter.getPosition(selectedWord))
+        selectedWordList = savedInstanceState.getStringArrayList("displayList")!!
+
+        displayList.clear()
+        displayList = selectedWordList
+    }
 }
--- app/src/main/java/com/example/androidktstarter461/MainActivity.kt
+++ app/src/main/java/com/example/androidktstarter461/MainActivity.kt
@@ -3,13 +3,14 @@ package com.example.androidktstarter461
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        startRadioDemo()
+//        startRadioDemo()
 
     }
 
@@ -20,13 +21,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     // starts the list demo
-    private fun startListDemo(){
+    fun startListDemo(view: View){
         val intent = Intent(this, ListDemo::class.java)
         startActivity(intent)
     }
 
     // starts the radio button demo
-    private fun startRadioDemo(){
+    fun startRadioDemo(view: View){
         val intent = Intent(this, RadioButtonDemo::class.java)
         startActivity(intent)
     }
